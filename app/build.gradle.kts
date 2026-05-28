60efe6b72a820a3c3557bd630a5528d54de0cc59
Sayantan Roy <sayantan.roy94@gmail.com>
2026-05-29 03:57:02 +0530

    feat: initialize Android application module with build configuration and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.github.codebydusk.nothanks"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
